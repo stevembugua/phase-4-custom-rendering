c397a93bf746f4d0433e51bb91cd6f0eedfa42ee
mbugua <mbuguafaimba@gmail.com>
2023-03-20 22:22:07 +0300

    completed assignment

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,15 +1,20 @@
 class CheesesController < ApplicationController
 
-  # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    #render json: cheeses, only: [:id, :name, :price, :is_best_seller]
+    render json: cheeses, except: [:created_at, :updated_at], methods: [:summary]
   end
 
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+    if cheese
+    render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+    else
+    render json: {error: "cheese not available"}, status: :not_found
+    end
   end
+
 
 end
